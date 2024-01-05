eb10753b2ca962a628ab5687fc04550674367f6f
camsdecker <camsdecker@gmail.com>
2024-01-04 20:39:04 -0500

    added /pics command

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# bot.py
 import os
 import discord
 from discord import app_commands
@@ -8,27 +7,24 @@ import bot_methods as bot
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD_ID = os.getenv('GUILD_ID')
 
-client = discord.Client(intents=discord.Intents.all())      #FIXME: limit the scope of the intents
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
-    #await tree.sync(guild=discord.Object(id=Your guild id))
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
     print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.content == 'test':
-        pic = bot.get_random_pic()
-        await client.get_user(message.author.id).send(file=discord.File(pic))
-
 @tree.command(
-    name="commandname",
-    description="My first application Command",
-    guild=discord.Object(id=12417128931)
+    name="pics",
+    description="Show a random pic",
+    guild=discord.Object(id=GUILD_ID)
 )
-async def first_command(interaction):
-    await interaction.response.send_message("Hello!")
+async def pics(interaction):
+    pic_path = bot.get_random_pic()
+    await interaction.response.send_message(file=discord.File(pic_path))
 
 client.run(TOKEN)
--- bot_methods.py
+++ bot_methods.py
@@ -1,15 +1,10 @@
 import os
 from random import randint
 
-def read_message(message):
-    if message.content == "/pic":
-        get_random_pic()
-    return
-
 def get_random_pic():
-    path = "C:/Users/camsd/Desktop/py_files/discord_bot/pics/"
+    path = "/home/cam/discord_bot/discord_bot/pics/"
     pic_list = os.listdir(path)
     chosen_pic_index = randint(0,len(pic_list)-1)
     chosen_pic = pic_list[chosen_pic_index]
 
-    return "C:/Users/camsd/Desktop/py_files/discord_bot/pics/" + chosen_pic
+    return path + chosen_pic
