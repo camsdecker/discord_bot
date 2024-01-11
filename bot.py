ab39554b2e1f7b35871e4a896fc6e017cd28cc27
camsdecker <camsdecker@gmail.com>
2024-01-11 17:44:22 -0500

    added /pause

--- bot.py
+++ bot.py
@@ -16,7 +16,6 @@ tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
-    #await tree.sync(guild=None)
     await tree.sync(guild=discord.Object(id=GUILD_ID))
     print(f'{client.user} has connected to Discord!')
 
@@ -30,9 +29,15 @@ async def on_ready():
     guild=discord.Object(id=GUILD_ID)
 )
 async def pics(interaction: discord.Interaction):
-    pic_path = bot.get_random_pic()
-    await interaction.response.send_message(file=discord.File(pic_path))
+    try:
+        pic_path = bot.get_random_pic()
+        await interaction.response.send_message(file=discord.File(pic_path))
+    except:
+        print(f"!!!ERROR: Something went wrong when sending file at {pic_path}!!!")
+        await interaction.response.send_message("ERROR: Something went wrong, try again")
 
+# /play
+# connects to the user's voice channel and plays the song from youtube_url
 @tree.command(
     name="play",
     description="play a song from youtube",
@@ -41,18 +46,40 @@ async def pics(interaction: discord.Interaction):
 async def play(interaction: discord.Interaction, youtube_url: str):
     if not interaction.user.voice:
         await interaction.response.send_message("ERROR: user is not in a voice channel")
-    else:
-        try:
-            if len(client.voice_clients) == 0:
-                bot_voice = await interaction.user.voice.channel.connect()
-            else:
-                bot_voice = client.voice_clients[0]
-            filename = await YTDLSource.from_url(youtube_url)
-            bot_voice.play(discord.FFmpegPCMAudio(executable="ffmpeg",source=filename))
-            await interaction.response.send_message(f"Now playing {youtube_url}")
-        except:
-            await interaction.response.send_message("ERROR: Something went wrong :(")
+        return
+    try:
+        if len(client.voice_clients) == 0:
+            bot_voice = await interaction.user.voice.channel.connect()
+        else:
+            bot_voice = client.voice_clients[0]
+        filename = await YTDLSource.from_url(youtube_url)
+        bot_voice.play(discord.FFmpegPCMAudio(executable="ffmpeg",source=filename))
+        await interaction.response.send_message(f"Now playing {youtube_url}")
+    except:
+        await interaction.response.send_message("ERROR: Something went wrong, try again")
 
+@tree.command(
+        name="pause",
+        description="pauses or resumes the music",
+        guild=discord.Object(id=GUILD_ID)
+)
+async def pause(interaction: discord.Interaction):
+    if len(client.voice_clients) == 0:
+        await interaction.response.send_message("ERROR: bot is not currently in a channel")
+        return
+    bot_voice = client.voice_clients[0]
+    if bot_voice.is_playing(): 
+        bot_voice.pause()
+        await interaction.response.send_message("Paused...")
+    elif bot_voice.is_paused():
+        bot_voice.resume()
+        await interaction.response.send_message("Resuming...")
+    else:
+        await interaction.response.send_message("ERROR: Bot is not playing any music")
+    
+
+# /leave
+# forces the bot to disconnect from the voice channel it's currently in
 @tree.command(
     name="leave",
     description="forces the bot to leave the voice channel",
@@ -63,6 +90,6 @@ async def leave(interaction: discord.Interaction):
         await interaction.response.send_message("ERROR: bot is not currently in a channel")
     else:
         await client.voice_clients[0].disconnect()
-        await interaction.response.send_message("Cam Bot out B)")
+        await interaction.response.send_message("Cambot out :sunglasses:")
 
 client.run(TOKEN)
