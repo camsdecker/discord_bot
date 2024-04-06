f86c64072c401b5a649e5cc91981d242e3a43e35
camsdecker <camsdecker@gmail.com>
2024-04-06 18:09:43 -0400

    added pic_list to Album.py

--- Album.py
+++ Album.py
@@ -5,16 +5,23 @@ import requests
 class Album:
     def __init__(self, dir):
         self.dir = dir
+        self.pic_list = os.listdir(dir)
 
     def addPic(self, pic):
         r = requests.get(pic)
         with open(self.dir + pic.filename, 'wb') as f:
             f.write(r.content)
 
+        self.pic_list.append(pic.filename)
+
     def getRandomPic(self):
-        path = self.dir
-        pic_list = os.listdir(path)
+        pic_list = self.pic_list
+        dir = self.dir
+        
         chosen_pic_index = randint(0,len(pic_list)-1)
         chosen_pic = pic_list[chosen_pic_index]
-        pic_path = path + chosen_pic
+        pic_path = dir + chosen_pic
+
+        pic_list.remove(chosen_pic)
+
         return pic_path
